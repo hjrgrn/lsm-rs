c6f926542523e292d9b9254bec0a7e23a3fc2762
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-01-03 15:26:38 +0100

    database: add new fields to Database
    - Add fields: max_memtable_size, memtable_size, sstables and sstable_counter
    - main: adapt existing code

--- src/database.rs
+++ src/database.rs
@@ -6,13 +6,17 @@ use std::hash::Hash;
 
 use serde::{Deserialize, Serialize};
 
-use crate::{memtable::MemTable, tombstone::Tombstone};
+use crate::{memtable::MemTable, sstable::SSTable, tombstone::Tombstone};
 
 pub struct Database<
     K: Serialize + for<'de> Deserialize<'de> + Ord + PartialEq + Eq + Hash + Clone,
     V: Tombstone + Clone + Serialize + for<'de> Deserialize<'de>,
 > {
     memtable: MemTable<K, V>,
+    max_memtable_size: usize,
+    memtable_size: usize,
+    sstables: Vec<SSTable>,
+    sstable_counter: usize,
 }
 
 // TODO: error
@@ -21,9 +25,13 @@ impl<
     V: Tombstone + Clone + Serialize + for<'de> Deserialize<'de>,
 > Database<K, V>
 {
-    pub fn build() -> Result<Self, String> {
+    pub fn build(max_memtable_size: usize) -> Result<Self, String> {
         Ok(Self {
             memtable: MemTable::new(),
+            max_memtable_size,
+            memtable_size: 0,
+            sstables: Vec::new(),
+            sstable_counter: 0,
         })
     }
 
