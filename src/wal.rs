7ddff5848f718e90865c7f9bdd970109c8c7c56f
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-01-05 11:48:20 +0100

    wal: Add WAL::write

--- src/wal.rs
+++ src/wal.rs
@@ -7,6 +7,8 @@ use std::{
     path::PathBuf,
 };
 
+use serde::Serialize;
+
 pub struct WAL {
     path: PathBuf,
     writer: BufWriter<File>,
@@ -16,5 +18,11 @@ impl WAL {
     pub fn build(path: PathBuf) -> Result<Self, io::Error> {
         let writer = BufWriter::new(File::options().append(true).create(false).open(&path)?);
         Ok(Self { path, writer })
+    }
+
+    // TODO: maybe other trait bounds
+    pub fn write<K: Serialize, V: Serialize>(&mut self, key: K, value: V) -> Result<(), io::Error> {
+        serde_json::to_writer(&mut self.writer, &(key, value))?;
+        Ok(())
     }
 }
