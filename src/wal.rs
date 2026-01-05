a370890258989a7efaea4fa5ceaa964b0a911143
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-01-05 12:07:05 +0100

    wal: Add WAL:replay_wal

--- src/wal.rs
+++ src/wal.rs
@@ -3,11 +3,14 @@
 
 use std::{
     fs::File,
-    io::{self, BufWriter},
+    hash::Hash,
+    io::{self, BufReader, BufWriter, ErrorKind},
     path::PathBuf,
 };
 
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+
+use crate::{memtable::MemTable, tombstone::Tombstone};
 
 pub struct WAL {
     path: PathBuf,
@@ -24,5 +27,28 @@ impl WAL {
     pub fn write<K: Serialize, V: Serialize>(&mut self, key: K, value: V) -> Result<(), io::Error> {
         serde_json::to_writer(&mut self.writer, &(key, value))?;
         Ok(())
+    }
+
+    pub fn replay_wal<
+        K: Ord + PartialEq + Eq + Hash + Clone + Serialize + for<'de> Deserialize<'de>,
+        V: Tombstone + Clone + Serialize + for<'de> Deserialize<'de>,
+    >(
+        path: &PathBuf,
+    ) -> Result<MemTable<K, V>, io::Error> {
+        let mut tab = MemTable::new();
+        let res = File::open(path);
+        if let Err(e) = res {
+            if e.kind() == ErrorKind::NotFound {
+                return Ok(tab);
+            } else {
+                return Err(e);
+            }
+        }
+        let mut reader = BufReader::new(File::open(path)?);
+        let data: Vec<(K, V)> = serde_json::from_reader(&mut reader)?;
+        for (k, v) in data.into_iter() {
+            let _ = tab.put(k, v);
+        }
+        Ok(tab)
     }
 }
