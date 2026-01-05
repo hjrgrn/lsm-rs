4ec3f2d5d44229229c68f0de026fd1d5d4b0de71
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-01-05 11:38:38 +0100

    wal: Add WAL::build

--- src/wal.rs
+++ src/wal.rs
@@ -1,9 +1,20 @@
 //! Write Ahead Log
 //! XXX:
 
-use std::{fs::File, io::BufWriter, path::PathBuf};
+use std::{
+    fs::File,
+    io::{self, BufWriter},
+    path::PathBuf,
+};
 
 pub struct WAL {
     path: PathBuf,
     writer: BufWriter<File>,
+}
+
+impl WAL {
+    pub fn build(path: PathBuf) -> Result<Self, io::Error> {
+        let writer = BufWriter::new(File::options().append(true).create(false).open(&path)?);
+        Ok(Self { path, writer })
+    }
 }
