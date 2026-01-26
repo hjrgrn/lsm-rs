e9041261f0872386d3d31d16ee8e2980dbc4ae4a
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-01-26 15:43:03 +0100

    manifest: Add field Manifest::path

--- src/manifest.rs
+++ src/manifest.rs
@@ -5,19 +5,25 @@ use std::{
     fs,
     io::{BufReader, BufWriter, ErrorKind},
     path::{Path, PathBuf},
+    str::FromStr,
 };
 
+const MANIFEST_PATH: &str = "./instance/MANIFEST";
+
 pub(crate) struct Manifest {
+    path: PathBuf,
     sstable_paths: Vec<PathBuf>,
 }
 
 impl Manifest {
     pub(crate) fn read_manifest(path: impl AsRef<Path>) -> AnyResult<Manifest> {
-        let f = match fs::File::open(path) {
+        let path = path.as_ref().to_path_buf();
+        let f = match fs::File::open(&path) {
             Ok(f) => f,
             Err(e) => {
                 if let ErrorKind::NotFound = e.kind() {
                     return Ok(Manifest {
+                        path,
                         sstable_paths: Vec::new(),
                     });
                 } else {
@@ -28,12 +34,15 @@ impl Manifest {
 
         let mut reader = BufReader::new(&f);
         let sstable_paths: Vec<PathBuf> = serde_json::from_reader(&mut reader)?;
-        Ok(Manifest { sstable_paths })
+        Ok(Manifest {
+            path,
+            sstable_paths,
+        })
     }
 
-    pub(crate) fn write_manifest(&self, path: impl AsRef<Path>) -> AnyResult<()> {
+    pub(crate) fn write_manifest(&self) -> AnyResult<()> {
         // TODO: use crate tempfile
-        let tmp_path = path.as_ref().to_path_buf().join(".tmp");
+        let tmp_path = self.path.join(".tmp");
         let f = fs::File::create(&tmp_path)?;
         let writer = BufWriter::new(&f);
         if let Err(e) = serde_json::to_writer(writer, &self.sstable_paths) {
@@ -41,7 +50,7 @@ impl Manifest {
             return Err(e.into());
         }
         // TODO: explain atomic rename
-        fs::rename(&tmp_path, path)?;
+        fs::rename(&tmp_path, &self.path)?;
 
         Ok(())
     }
